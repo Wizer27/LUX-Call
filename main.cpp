83a6acaa9795d2a18b199e73933299e8aa740fdb
Wizer27 <svd2yar@gmail.com>
2025-10-12 23:14:52 +0300

    Init functions

--- main.cpp
+++ main.cpp
@@ -5,11 +5,47 @@
 #include <chrono>
 #include <thread>
 #include <string>
-#include <rtc/rtc.hpp>
+#include "/Users/ivan/vcpkg/installed/arm64-osx/include/rtc/rtc.hpp" //chacnge to your path
 
 
 
 using namespace std;
+std::shared_ptr<rtc::PeerConnection> pc;
+
+class VideoCall {
+public:
+    void start_as_caller() {
+        cout << "STARTING" << endl;
+        setupPeerConnection();
+        addMediaTracks();
+        createOffer();
+        waitForCall();
+        pc -> setLocalDescription();
+    }
+    void start_as_callee() {
+        cout << "STARTING AS CALLEE";
+        setupPeerConnection();
+        addMediaTracks();
+        waitForOffer();
+    }
+private:
+    void setupPeerConnection(){
+
+    }   
+    void addMediaTracks(){
+
+    } 
+    void createOffer(){
+
+    }
+    void waitForCall(){
+
+    }
+    void waitForOffer(){
+
+    }
+};
+
 int main() {
     cout << "TEST";
     return 0;

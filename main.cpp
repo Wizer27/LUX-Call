d6af9925af69924615dc8b706abf8779e6281f1a
Wizer27 <svd2yar@gmail.com>
2025-10-12 22:05:32 +0300

    C++ init

--- main.cpp
+++ main.cpp
@@ -2,6 +2,17 @@
 #include <algorithm>
 #include <vector>
 #include <unordered_map>
+#include <chrono>
+#include <thread>
+#include <string>
+#include <rtc/rtc.hpp>
+
+
 
 using namespace std;
+int main() {
+    cout << "TEST";
+    return 0;
+}
+
 

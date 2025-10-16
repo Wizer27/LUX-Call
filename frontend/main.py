7d100484efaebaad5ba7fa998144fe03ba1c1353
Wizer27 <svd2yar@gmail.com>
2025-10-16 23:14:41 +0300

    Samll fix

--- frontend/main.py
+++ frontend/main.py
@@ -3,6 +3,7 @@ import json
 import requests 
 import hashlib
 import hmac
+import time
 
 
 
@@ -35,6 +36,13 @@ def get_secret_key() -> str:
     except KeyError:
         return "Error"    
 
+def get_api_key() -> str:
+    with open("data/secrets.json","r") as file:
+        data = json.load(file)
+    try:
+        return data["key"]
+    except KeyError:
+        return "Error"    
     
 
 def register_new_user(username:str,hash_password:str) -> bool:
@@ -44,6 +52,23 @@ def register_new_user(username:str,hash_password:str) -> bool:
             "username":username,
             "password":hash_password
         }
+        main_siganature = GenerateSignature(get_secret_key(),get_api_key())
+        json_data = json.dumps(data)
+        timestamp = str(int(time.time()))
+        signature = main_siganature.generate(json_data,timestamp)
+        headers = {
+            "Content-Type": "application/json",
+            "X-Signature": signature,
+            "X-Timestamp": timestamp,
+            "X-API-Key": get_api_key()
+        }
+        try:
+            resp = requests.post(url,json = data,headers=headers)
+            #DEBUG
+            print(resp.status_code)
+            print(resp.text)
+        except Exception as e:
+            raise Exception 
     except Exception as e:
         return False
 

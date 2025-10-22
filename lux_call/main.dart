373764c0962abd03d8ecca99c106a6e482eaf7d5
Wizer27 <svd2yar@gmail.com>
2025-10-22 11:46:10 +0300

    ?

--- lux_call/main.dart
+++ lux_call/main.dart
@@ -66,6 +66,8 @@ void init() async {
   String api_key = secrets_data["api"];
   String secret_key = secrets_data["key"];
   siganture_middleware = GenerateSignature(BaseUrl: "http://0.0.0.0:80", api_key: api_key, secret_key: secret_key);
+
+  
 }
 
 

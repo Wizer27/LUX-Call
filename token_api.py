5e78861b80f54c666a10677a7be97b1c4a7e80b5
Wizer27 <svd2yar@gmail.com>
2025-10-29 21:30:10 +0100

    Regsiter endpoint

--- token_api.py
+++ token_api.py
@@ -1,9 +1,39 @@
-from fastapi import FastAPI,HTTPException
+from fastapi import FastAPI,HTTPException,Depends
 import jwt
 from pydantic import BaseModel,Field
 from secrets import compare_digest
 import json
 import time
+import hmac
+import uuid
+import hashlib
+import uvicorn
+
+
+
+#---- INIT ----
+users_file = "data/users.json"
+
+
+
+def get_siganture_key() -> str:
+    with open("secrets.json","r") as file:
+        data = json.load(file)
+    return data["sign"]    
+
+def verify_signature(data: dict, received_signature: str) -> bool:
+    if time.time() - data.get('timestamp', 0) > 300:
+        return False
+    
+    
+    data_to_verify = data.copy()
+    data_to_verify.pop("signature", None)
+    
+    data_str = json.dumps(data_to_verify, sort_keys=True, separators=(',', ':'))
+    expected_signature = hmac.new(KEY.encode(), data_str.encode(), hashlib.sha256).hexdigest()
+    
+
+
 
 def get_secret() -> str:
     with open("secrets.json","r") as file:
@@ -31,3 +61,40 @@ async def generate(request:Generate_JWT):
         return token
     except Exception as e:
         raise HTTPException(status_code = 400,detail = f"Error : {e}")   
+class Register(BaseModel):
+    username:str
+    psw:str
+    signature:str
+    timestamp:str
+@app.post("/register")
+async def register(request:Register):
+    #binary search
+    def is_user_exists(username:str,data) -> bool:
+        l = 0
+        r = len(data.keys()) - 1
+        keys = []
+        for key in data.keys():
+            keys.append(key)
+        while l <= r:
+            mid = (l + r) // 2
+            if keys[mid] == username:
+                return True
+            elif keys[mid] < username:
+                l = mid + 1
+            elif keys[mid] > username:
+                r = mid - 1
+        return False            
+        
+    if not verify_signature(request.model_dump(),request.signature):
+        raise HTTPException(status_code = 401,detail = "Invalid signature")
+    try:
+        with open(users_file,"r") as file:
+            data = json.load(file)
+        if is_user_exists(request.username,data):
+            raise HTTPException(status_code = 400,detail = "This username is already taken")  
+        else:
+            data[request.username] = request.psw
+            with open(users_file,"w") as file:
+                json.dump(data,file)  
+    except Exception as e:
+        raise HTTPException(status_code = 400,detail = f"Error : {e}")    

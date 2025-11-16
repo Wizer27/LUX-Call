9a769bfd7ffa0b8a1de34cc8b3fa616264a53ba4
Wizer27 <svd2yar@gmail.com>
2025-11-16 22:30:24 +0300

    Crypto in C++

---
+++ crypto/cypto.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <unordered_map>
+#include <nlohmann/json.hpp>
+#include <iomanip>
+#include <random>
+#include <algorithm>
+#include <ctime>
+#include <cmath>
+#include <stdexcept>
+#include <string>
+#include <vector>
+#include <functional>
+
+
+using namespace std;
+using json = nlohmann::json;
+
+
+class Hashing{
+public:
+    size_t get_hash(string mess){
+        return std::hash<std::string>()(mess);
+    }
+};
+
+int main(){
+    return 0;
+}
+

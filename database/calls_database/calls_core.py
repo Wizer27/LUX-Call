19f5679932808a881a4eedcdd4598acdb521b961
Wizer27 <svd2yar@gmail.com>
2025-12-20 00:06:43 +0300

    write new call && some fixes

--- database/calls_database/calls_core.py
+++ database/calls_database/calls_core.py
@@ -2,6 +2,8 @@ from calls_models import metadata_obj,calls_table
 from calls_sqli import sync_engine
 from sqlalchemy import select,update,delete,or_
 from typing import List
+from datetime import datetime
+import uuid
 
 def create_table():
     metadata_obj.create_all(sync_engine)
@@ -13,4 +15,18 @@ def get_all_data():
             return res.fetchall()
         except Exception as e:
             return Exception(f"Error : {e}")
-print(get_all_data())        
+def write_new_call(username:str,to_user:str,call_type:str,time:int):
+    with sync_engine.connect() as conn:
+        try:
+            stmt = calls_table.insert().values(
+                id = str(uuid.uuid4()),
+                username = username,
+                called_to = to_user,
+                date = str(datetime.now()).split()[0],
+                call_type = call_type,
+                time = time
+            )
+            conn.execute(stmt)
+            conn.commit()
+        except Exception as e:
+            return Exception(f"Call error {e}")     
--- database/calls_database/calls_models.py
+++ database/calls_database/calls_models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import text,String,Column,Boolean,MetaData,ARRAY,Table
+from sqlalchemy import text,String,Column,Boolean,MetaData,ARRAY,Table,Integer
 from sqlalchemy.dialects.postgresql import JSONB
 
 
@@ -8,8 +8,9 @@ calls_table = Table(
     "calls_data",
     metadata_obj,
     Column("id",String,primary_key=True),
+    Column("username",String),
     Column("called_to",String),
     Column("date",String),
-    Column("call_type",String)
-
+    Column("call_type",String),
+    Column("time",Integer)
 )
